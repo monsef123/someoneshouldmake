ad43f6153c2288eb6a4ea3d8f33ad9cd997a19eb
Mounsif Meftah <mounsifmeftah@tookeez.com>
2024-05-22 16:54:31 +0100

    Add posts list

--- app/components/post-card/index.tsx
+++ app/components/post-card/index.tsx
@@ -4,12 +4,14 @@ interface PostCardProps {
 	title: string;
 	description: string;
 	likes: number;
+	donations: number;
+	submissions: number;
 	// ...
 }
 
-export default function PostCard({ title, description, likes }: PostCardProps) {
+export default function PostCard({ title, description, likes, donations, submissions }: PostCardProps) {
 	return (
-		<div className="bg-background-light p-4 box-border rounded-lg w-full">
+		<div className="bg-background-light p-4 box-border rounded-lg w-full mt-4">
 			<h1 className="text-base font-medium text-primary-light">{title}</h1>
 			<p className="text-sm text-primary-light">{description}</p>
 			<div className="flex items-center justify-between mt-2">
@@ -21,7 +23,7 @@ export default function PostCard({ title, description, likes }: PostCardProps) {
 							width={20}
 							height={20}
 						/>
-						10
+						{likes}
 					</button>
 					<button className="flex items-center bg-transparent gap-1 text-primary-light">
 						<Image
@@ -34,8 +36,8 @@ export default function PostCard({ title, description, likes }: PostCardProps) {
 					</button>
 				</div>
 				<div className="flex items-center justify-end gap-4">
-					<p className="font-medium text-secondary-light text-sm">10$ Donated</p>
-					<p className="font-medium text-primary-light text-sm">2 Submissions</p>
+					<p className="font-medium text-secondary-light text-sm">{donations}$ Donated</p>
+					<p className="font-medium text-primary-light text-sm">{submissions} Submissions</p>
 				</div>
 			</div>
 		</div>
--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -12,11 +12,48 @@ export default function ExplorePage() {
 					<div className="col-span-1 h-60 bg-secondary-light rounded-lg">
 
 					</div>
-					<div className="col-span-3 h-60 bg-primary-light rounded-lg box-border p-4">
+					<div className="col-span-3 bg-primary-light rounded-lg box-border p-4 pt-0">
 						<PostCard 
 							title="Easier OTA solution for react native"
 							description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..."
 							likes={300}
+							donations={10}
+							submissions={2}
+						/>
+						<PostCard 
+							title="Easier OTA solution for react native"
+							description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..."
+							likes={300}
+							donations={10}
+							submissions={2}
+						/>
+						<PostCard 
+							title="Easier OTA solution for react native"
+							description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..."
+							likes={300}
+							donations={10}
+							submissions={2}
+						/>
+						<PostCard 
+							title="Easier OTA solution for react native"
+							description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..."
+							likes={300}
+							donations={10}
+							submissions={2}
+						/>
+						<PostCard 
+							title="Easier OTA solution for react native"
+							description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..."
+							likes={300}
+							donations={10}
+							submissions={2}
+						/>
+						<PostCard 
+							title="Easier OTA solution for react native"
+							description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..."
+							likes={300}
+							donations={10}
+							submissions={2}
 						/>
 					</div>
 				</div>
